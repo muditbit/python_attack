21783cd3ee20e031d1d70213a0a81496b00f8d55
Mudit Srivastava <60660510+muditbit@users.noreply.github.com>
2021-10-10 17:19:27 +0530

    Update pass_the_baton.py

--- Competetion/pass_the_baton.py
+++ Competetion/pass_the_baton.py
@@ -1,4 +1,14 @@
-'''Question :- '''
+'''Question :- There are n friends
+ standing in a line, each numbered from 1
+through n inclusive. The first one, 
+friend 1, holds a baton. 
+Each second, the baton is passed to 
+the next friend in line. 
+Once it reaches the end of the line, 
+the direction of passing is reversed 
+and passing continues. 
+Determine who will pass and who 
+will receive the baton at a given time.'''
 
 n = int(input())
 t= int(input())
