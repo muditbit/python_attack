a5ff138a68bf5617b75f8843fad1ec01be86c6f9
muditbit <srivastavamudit560@gmail.com>
2021-10-10 17:24:37 +0530

    Updated Question

--- Competetion/pass_the_baton.py
+++ Competetion/pass_the_baton.py
@@ -1,13 +1,9 @@
 '''Question :- There are n friends
  standing in a line, each numbered from 1
 through n inclusive. The first one, 
-friend 1, holds a baton. 
-Each second, the baton is passed to 
-the next friend in line. 
-Once it reaches the end of the line, 
-the direction of passing is reversed 
-and passing continues. 
-Determine who will pass and who 
+friend 1, holds a baton.  Each second, the baton is passed to the next friend in line. 
+Once it reaches the end of the line, the direction of passing is reversed 
+and passing continues.Determine who will pass and who 
 will receive the baton at a given time.
 
 
@@ -18,12 +14,9 @@ friends=4
 time = 5
 
 Friends are numbered 1 
-through 4. Friend 1 holds the 
-baton at time 0. At time 1,
- it is passed to friend 2. 
-Over 5 seconds, 
-the baton is passed as 1-2-3-4-3->2. 
-The friend passing the baton at time 5 is
+through 4. Friend 1 holds the baton at time 0. At time 1,
+ it is passed to friend 2. Over 5 seconds, 
+the baton is passed as 1-2-3-4-3->2. The friend passing the baton at time 5 is
 friend 3. The friend receiving 
 the baton is friend 2. Return [3, 2].'''
 
